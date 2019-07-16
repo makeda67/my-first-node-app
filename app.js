7cdf29b23177b9bcff0310b6a826838b8e5a9aaa
Aleix Maqueda <aleixmaqueda97@gmail.com>
2019-07-16 11:42:36 +0200

    My first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+const chalk = require('chalk');
+const getNames = require('./myModule')
+
+const students = [
+    {name: 'Anna',
+    age: 28},
+    {name: 'Paolo',
+    age: 27},
+];
+
+console.log(chalk.blue(getNames(students)));
